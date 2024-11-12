5c10f6db19f4647ab58106a76b48eaa06be3b435
niitenberg <drbrain@aol.com>
2024-11-12 10:49:35 +0200

    New code

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello universe!");
+        String message = "Welcome to the universe";
+        System.out.println(message);
 
     }
 }
