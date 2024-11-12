372fb82141d23c4f901363ecd52d8e5eb5a8a5c1
niitenberg <drbrain@aol.com>
2024-11-12 09:51:55 +0200

    Feature branc first commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello people!");
+        String text = "New world";
+
+        System.out.println(text);
 
     }
 }
