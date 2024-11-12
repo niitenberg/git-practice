f9c785af984558641cb77855a2af5cb1ace769a2
niitenberg <drbrain@aol.com>
2024-11-12 09:35:54 +0200

    Initial project commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        var nums= IntStream.range(1,51).boxed().toList();
+
     }
 }
